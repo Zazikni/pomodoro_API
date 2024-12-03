04d8b3b1e13a9306ff67a3eebc6fe423ee104435
zazik <nikitachepelskii2@gmail.com>
2024-12-03 16:09:53 +0300

    extend settings model

--- core/settings.py
+++ core/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DB_URL: str = "sqlite+aiosqlite:///./pomodoro_database.sqlite3"
+    SQLALCHEMY_ECHO: bool = True
 
 
 settings = Settings()
