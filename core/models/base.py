ebcc6e935151e80cd67316709a46f53e685f00cc
zazik <nikitachepelskii2@gmail.com>
2024-12-03 20:02:41 +0300

    add autoincrement to base sqlalchemy model

--- core/models/base.py
+++ core/models/base.py
@@ -8,4 +8,4 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"  # Имя таблицы будет именем модели в нижнем регистре
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
