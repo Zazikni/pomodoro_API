4620b3090b40e1915081dd75178806979aba803f
zazik <nikitachepelskii2@gmail.com>
2024-12-03 19:27:34 +0300

    add crud for tasks

---
+++ api_v1/tasks/crud.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from core.models import Task
+from .schemas import TaskCreate
+
+
+async def get_all_tasks(session: AsyncSession) -> list[Task]:
+    statement = select(Task).order_by(Task.id)
+    result: Result = await session.execute(statement)
+    tasks: list[Task] = list(result.scalars().all())
+    return tasks
+
+
+async def get_one_task(session: AsyncSession, task_id: int) -> Task | None:
+    return await session.get(Task, task_id)
+
+
+async def create_task(session: AsyncSession, task: TaskCreate) -> Task:
+    new_task = Task(**task.model_dump())
+    session.add(new_task)
+    await session.commit()
+    await session.refresh(new_task)
+    return new_task
