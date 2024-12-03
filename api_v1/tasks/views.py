a7582cafde703aa0570f18f40cc853bbb2b7b6e4
zazik <nikitachepelskii2@gmail.com>
2024-12-03 20:27:41 +0300

    add interaction with the database

--- api_v1/tasks/views.py
+++ api_v1/tasks/views.py
@@ -1,51 +1,52 @@
-from typing import List
+from fastapi import APIRouter, status, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from fastapi import APIRouter, status, HTTPException
 from api_v1.tasks.schemas import Task, TaskCreate, TaskUpdateTitle, TaskDelete
+from . import crud
+from core.database_manager import database_manager
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
-
-default_task = Task(title="default_task_title", pomodoro_count=0, category_id=1, id=1)
-default_tasks_list = [default_task]
 
 
 @router.get(
     "/all",
-    response_model=List[Task],
+    response_model=list[Task],
     status_code=status.HTTP_200_OK,
     responses={
         status.HTTP_404_NOT_FOUND: {"description": "Tasks not found"},
     },
     description="Endpoint to get all tasks",
 )
-async def get_tasks():
-    return default_tasks_list
+async def get_tasks(
+    session: AsyncSession = Depends(database_manager.session_dependency),
+) -> list[Task]:
+    return await crud.get_all_tasks(session=session)
 
 
 @router.post(
     "/",
     response_model=Task,
 )
-async def create_task(task_data: TaskCreate):
-    task = Task(
-        title=task_data.title,
-        pomodoro_count=task_data.pomodoro_count,
-        category_id=task_data.category_id,
-        id=len(default_tasks_list),
-    )
-    default_tasks_list.append(task)
-    return task
+async def create_task(
+    task_data: TaskCreate,
+    session: AsyncSession = Depends(database_manager.session_dependency),
+):
+    return await crud.create_task(session=session, task=task_data)
 
 
 @router.post(
     "/edit/title",
     response_model=Task,
 )
-async def edit_task_title(task_info: TaskUpdateTitle):
-    for task in default_tasks_list:
-        if task.id == task_info.id:
-            task.title = task_info.title
-            return task
+async def edit_task_title(
+    task_info: TaskUpdateTitle,
+    session: AsyncSession = Depends(database_manager.session_dependency),
+):
+    success = await crud.edit_task_title(
+        session=session, task_id=task_info.id, title=task_info.title
+    )
+    if success:
+        return await crud.get_one_task(session=session, task_id=task_info.id)
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
@@ -59,13 +60,14 @@ async def edit_task_title(task_info: TaskUpdateTitle):
         status.HTTP_404_NOT_FOUND: {"description": "Task not found"},
     },
     description="Endpoint to get all tasks",
-    # response_model=Task,
 )
-async def delete_task(task_info: TaskDelete):
-    for index, task in enumerate(default_tasks_list):
-        if task.id == task_info.id:
-            default_tasks_list.remove(task)
-            return {"message": f"Task with id: {task_info.id} successfully deleted"}
+async def delete_task(
+    task_info: TaskDelete,
+    session: AsyncSession = Depends(database_manager.session_dependency),
+):
+    success = await crud.delete_task(session=session, task_id=task_info.id)
+    if success:
+        return {"message": f"Task with id: {task_info.id} successfully deleted"}
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
