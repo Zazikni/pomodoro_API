05f14830bdc8b027222daa00e3f85adaaf31de48
zazik <nikitachepelskii2@gmail.com>
2025-02-19 20:03:24 +0300

    users views reworked

--- api_v1/users/views.py
+++ api_v1/users/views.py
@@ -2,25 +2,12 @@ from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api_v1.users.schemas import User, UserEdit, UserCreate
-from .dependencies import get_user_by_id_from_body
+from .dependencies import get_user_by_id_from_path
 from core.database_manager import database_manager
 from api_v1.users import crud
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
-
-
-@router.get(
-    "/",
-    response_model=User,
-    status_code=status.HTTP_200_OK,
-    responses={status.HTTP_404_NOT_FOUND: {"description": "User not found"}},
-    description="Endpoint to get a user",
-)
-async def get_user(
-    user: User = Depends(get_user_by_id_from_body),
-):
-    return user
 
 
 @router.post(
@@ -37,14 +24,27 @@ async def create_user(
     return user
 
 
+@router.get(
+    "/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    responses={status.HTTP_404_NOT_FOUND: {"description": "User not found"}},
+    description="Endpoint to get a user",
+)
+async def get_user(
+    user: User = Depends(get_user_by_id_from_path),
+):
+    return user
+
+
 @router.delete(
-    "/",
+    "/{user_id}",
     status_code=status.HTTP_204_NO_CONTENT,
     responses={status.HTTP_404_NOT_FOUND: {"description": "User not found"}},
     description="Endpoint to delete user",
 )
 async def delete_user(
-    user: User = Depends(get_user_by_id_from_body),
+    user: User = Depends(get_user_by_id_from_path),
     session: AsyncSession = Depends(database_manager.scoped_session_dependency),
 ):
     await crud.delete_user(session=session, user=user)
