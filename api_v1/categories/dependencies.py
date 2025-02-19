29c66f676795b0e7c3d76fd3c41780f7ae6ea37a
zazik <nikitachepelskii2@gmail.com>
2025-02-19 19:56:19 +0300

    categories dependencies reworked

--- api_v1/categories/dependencies.py
+++ api_v1/categories/dependencies.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, HTTPException, status, Path
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api_v1.categories.schemas import Category, CategoryCreate, CategoryEdit
+from api_v1.categories.schemas import CategoryEdit
 from core.database_manager import database_manager
 from . import crud
 
@@ -10,22 +10,30 @@ async def get_category_by_id_from_body(
     category_data: CategoryEdit,
     session: AsyncSession = Depends(database_manager.scoped_session_dependency),
 ):
-    category = await crud.get_category(session=session, category_id=category_data.id)
-    if not category:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Category with id: {category_data.id} not found",
-        )
 
-    return category
+    return await get_category_by_id(session=session, category_id=category_data.id)
 
 
 async def get_category_by_id_from_path(
     category_id: int = Path(..., gt=0),
     session: AsyncSession = Depends(database_manager.scoped_session_dependency),
 ):
-    category = await get_category_by_id_from_body(
-        category_data=CategoryEdit(id=category_id), session=session
+
+    return await get_category_by_id(session=session, category_id=category_id)
+
+
+async def get_category_by_id(
+    session: AsyncSession,
+    category_id: int,
+):
+    category = await crud.get_category(
+        session=session,
+        category_id=category_id,
     )
+    if not category:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Category with id: {category_id} not found",
+        )
 
     return category
